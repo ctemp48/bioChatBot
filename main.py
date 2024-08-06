a95f4a5fdf89970284778679a988f1f61483bd3c
ctemp48 <cltwheels@gmail.com>
2024-08-06 17:44:47 -0400

    v1.1

--- main.py
+++ main.py
@@ -7,6 +7,12 @@ from langchain.prompts.prompt import PromptTemplate
 from dotenv import load_dotenv
 from langchain.schema.runnable import RunnablePassthrough
 from langchain.schema.output_parser import StrOutputParser
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_community.chat_message_histories import ChatMessageHistory
 import os
 
 class Chatbot():
@@ -26,28 +32,73 @@ class Chatbot():
                         temperature=0.1,
                         top_k=50, 
                         huggingfacehub_api_token=os.getenv('HUGGINGFACE_ACCESS_TOKEN'))
+    
 
-    template = """
-    You are answering questions about a person named Christian. Use the provided 
-    context to answer these questions. If the context does not provide an answer, just 
-    say that you do not know. Do not answer any questions that are not relevant to Christian.
-
-    Context: {context}
-    Question: {question}
-    Answer:
-
-    """
-
-    prompt = PromptTemplate(template = template, input_variables=["context", "question"])
-
-    rag_chain = (
-    {"context": db.as_retriever(),  "question": RunnablePassthrough()} 
-    | prompt 
-    | llm
-    | StrOutputParser() 
+    ### Contextualize question ###
+    contextualize_q_system_prompt = """Given a chat history and the latest user question \
+    which might reference context in the chat history, formulate a standalone question \
+    which can be understood without the chat history. Do NOT answer the question, \
+    just reformulate it if needed and otherwise return it as is."""
+    contextualize_q_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", contextualize_q_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    history_aware_retriever = create_history_aware_retriever(
+        llm, db.as_retriever(), contextualize_q_prompt
     )
 
+    qa_system_prompt = """
+     You are answering questions about a person named Christian. Use the provided 
+    context to answer these questions. If the context does not provide an answer, just 
+    say that you do not know. Do not answer any questions that are not relevant to Christian.
+    {context}
+    """
+    qa_prompt = ChatPromptTemplate.from_messages(
+        [
+            ("system", qa_system_prompt),
+            MessagesPlaceholder("chat_history"),
+            ("human", "{input}"),
+        ]
+    )
+    question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+
+    rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+    store = {}
+
+    
+    def get_session_history(session_id: str, store=store) -> BaseChatMessageHistory:
+        if session_id not in store:
+            store[session_id] = ChatMessageHistory()
+        return store[session_id]
+    
+    conversational_rag_chain = RunnableWithMessageHistory(
+        rag_chain,
+        get_session_history,
+        input_messages_key="input",
+        history_messages_key="chat_history",
+        output_messages_key="answer",
+    )
+
+
+
 bot = Chatbot()
-input=input("Ask me anything: ")
-result = bot.rag_chain.invoke(input)
-print(result)
+
+result = bot.conversational_rag_chain.invoke(
+    {"input": "What is Christian's favorite color?"},
+    config={
+        "configurable": {"session_id": "abc123"}
+    },  # constructs a key "abc123" in `store`.
+)
+print(result["answer"])
+
+result = bot.conversational_rag_chain.invoke(
+    {"input": "What is the color that you just said?"},
+    config={
+        "configurable": {"session_id": "abc123"}
+    },  # constructs a key "abc123" in `store`.
+)
+print(result["answer"])
